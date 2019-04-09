90b08a3064167f5d794652845176754a064fee3e
ktosox <k>
2019-04-09 19:35:53 +0200

    player now fades in/out on mode cahnge

--- Scenes_and_Scripts/Player/PlayerGhost.gd
+++ Scenes_and_Scripts/Player/PlayerGhost.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 var followMouse = false
 var lockSpeed = 30
 var followSpeed = 50
+var resizeSpeed = 2
 var restLocation = Vector2(500,500)
 
 func _ready():
@@ -13,9 +14,14 @@ func _ready():
 
 func _process(delta):
 	if(followMouse):
+		if($Trail2.modulate.a<1.0):
+			$Trail2.modulate.a+=delta * resizeSpeed
 		linear_velocity =  (get_global_mouse_position() - position) * (followSpeed * delta)
 	else:
 		linear_velocity = (restLocation - position) / (lockSpeed * delta)
+		if($Trail2.modulate.a>0.1):
+			$Trail2.modulate.a-=delta * resizeSpeed
+
 
 func _on_SpawnDelay_timeout():
 	$Trail.visible = true
